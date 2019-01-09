223f52535da12fa965f5ad98333de9e02984c717
BLian <brianlian38@users.noreply.github.com>
2019-01-09 10:52:49 -0500

    Tank Client Upload
    Client that detects inputs and sends out  proper network data

---
+++ TankClientPanel.java
@@ -0,0 +1,49 @@
+//Tank Client Panel
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+
+public class TankClientPanel extends JPanel{
+	// Properties
+	int intBallX = 100;
+	int intBallY = 100;
+	boolean blnGoDown = false;
+	boolean blnGoUp = false;
+	boolean blnGoLeft = false;
+	boolean blnGoRight = false;
+	
+	// Methods
+	public void paintComponent(Graphics g){ // By default, JPanel draws a blank panel. We are overriding it so it's no longer drawing a blank screen.
+		// White Rectangle Object (Background)
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 1280, 720); 
+
+		// Blue Oval Object
+		g.setColor(Color.BLUE);
+		g.fillOval(intBallX, intBallY, 20, 20);
+		//intBallX = intBallX + 1; // Makes the ball X-Coordinate +1 each time panel is refreshed.
+		if(blnGoDown){ // Translation movement of Oval Object using Boolean.
+			intBallY++;
+		}
+		if(blnGoUp){
+			intBallY--;
+		}
+		if(blnGoLeft){
+			intBallX--;
+		}
+		if(blnGoRight){
+			intBallX++;
+		}
+	}
+	
+	
+	// Constructors
+	public TankClientPanel(){
+		super();
+	}
+	
+}
+
