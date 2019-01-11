d0d177fec2e075cd81cebd127763dd378e3e0779
KevinHioe <45692619+KevinHioe@users.noreply.github.com>
2019-01-11 10:59:22 -0500

    Update GameServer.java

--- GameServer.java
+++ GameServer.java
@@ -31,7 +31,44 @@ public class GameServer implements ActionListener{
 
 		if(e.getSource() == timer){
 			//use vars here, calculate movement and send to clients
-
+			//Split client data
+			//Apply Calculation
+			String strCData;
+			int intUp;
+			int intLeft;
+			int intDown;
+			int intRight;
+			
+			strCData = ssm.readText();
+			String[] clientData = strCData.split(",");
+			//Check client IP
+			if(strCData[0] == getMyAddress){
+				if(strCData[1] == "MvmtUt"){
+					intUp = intUp + 10;
+				}else if(strCData[1] == "MvmtUf"){
+					intUp = intUp;
+				}if(strCData[2] == "MvmtLt"){
+					intLeft = intLeft + 10;
+				}else if(strCData[2] == "MvmtLf"){
+					intLeft = intLeft;
+				}if(strCData[3] == "MvmtDt"){
+					intDown = intDown + 10;
+				}else if(strCData[3] == "MvmtDf"){
+					intDown = intDown;
+				}if(strCData[4] == "MvmtRt"){
+					intRight = intRight + 10;
+				}else if(strCData[4] == "MvmtRf"){
+					intRight = intRight;
+				}if(strCData[5] == "Shott"){
+					System.out.println("Shot Fired");
+				}else if(strCData[5] == "Shotf"){
+					System.out.println("Shot Not Fired");
+				}if(strCData[6] == mouseX){
+					System.out.println("Turret Angled X Coordinate");
+				}if(strCData[7] == mouseY){
+					System.out.println("Turret Angled Y Coordinate");
+				}
+			}
 		}
 	}
 
