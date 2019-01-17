659628f67e961631867eff31ea8678b7b8a868d0
KevinHioe <45692619+KevinHioe@users.noreply.github.com>
2019-01-17 11:03:02 -0500

    Put mainMenu image

--- mainPanel.java
+++ mainPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.*;
+import java.awt.event.*;
 import java.io.*;
 import javax.imageio.*;
 import java.awt.Font.*;
@@ -10,12 +11,12 @@ public class mainPanel extends JPanel{
 	boolean blnStart = false;
 	boolean blnMM = true;
 	boolean blnHelp = false;
+	BufferedImage mainmenu;
 	// Methods
 	public void paintComponent(Graphics g){
 		// Background
 		if(blnMM == true){
-			g.setColor(Color.WHITE);
-			g.fillRect(0, 0, 1280, 720);
+			g.drawImage(mainmenu, 0, 0, null);
 		}else if(blnHelp == true){
 			g.setColor(Color.RED);
 			g.fillRect(0, 0, 1280, 720);
@@ -24,5 +25,11 @@ public class mainPanel extends JPanel{
 	// Constructors
 	public mainPanel(){
 		super();
+		try{
+			mainmenu = ImageIO.read(new File("Tank Menu.png"));
+		}catch(IOException e){
+			System.out.println("Unable to load image");
+		}
 	}
 }
+
