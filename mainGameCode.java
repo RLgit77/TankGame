8ba5579f7c17d33c7ecdf7dad0230192b8abacee
KevinHioe <45692619+KevinHioe@users.noreply.github.com>
2019-01-20 00:26:09 -0500

    Updated menu buttons
    server and client button added plus visible border

--- mainGameCode.java
+++ mainGameCode.java
@@ -2,25 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class mainMenu implements ActionListener{
+public class mainGameCode implements ActionListener{
 	// Properties
 	JFrame frame;
 	mainPanel panel;
-	JButton play; // Play Button in MM
+	JButton client; // Client Button in MM
+	JButton server; // Server Button in MM
 	JButton help; // ? in MM
 	JButton backtomenu; // Arrow Buttons that lead back to MM
 	JButton backtocontrols; // Arrow Button that lead to controls
 	JButton nexttopandt; // Arrow Button that leads to powerup and tiles
 	JButton backtomenu2; // Arrow Buttons that lead back to MM
 	Timer timer;
-	
-	
+		
 	// Methods
+	// Button Direct Interaction
 	public void actionPerformed(ActionEvent e){
-		if(e.getSource() == play){
+		// IN MAIN MENU:
+		if(e.getSource() == client){
 			// Insert code which leads to game client screen
-			// Set help button to false so there is no interferences
-			System.out.println("Running Game");
+			System.out.println("Starting a client");
+			// Disable Buttons
+			client.setVisible(false);
+			server.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
+		}else if(e.getSource() == server){
+			// Insert code which leads to game server screen
+			System.out.println("Starting a server");
+			// Disable Buttons
+			client.setVisible(false);
+			server.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
 			// If you press "?":	
 		}else if(e.getSource() == help){
 			// Screens:			
@@ -28,7 +48,9 @@ public class mainMenu implements ActionListener{
 			panel.blnControls = true;
 			panel.blnPandT = false;
 			// Buttons:
-			play.setVisible(false);
+			server.setVisible(false);
+			client.setVisible(false);
+			server.setVisible(false);
 			help.setVisible(false);
 			backtocontrols.setVisible(false);
 			nexttopandt.setVisible(true);
@@ -43,11 +65,12 @@ public class mainMenu implements ActionListener{
 			panel.blnControls = false;
 			panel.blnPandT = false;
 			// Buttons:
-			play.setVisible(true);
+			server.setVisible(true);
+			client.setVisible(true);
 			help.setVisible(true);
 			backtocontrols.setVisible(false);
-			nexttopandt.setVisible(true);
-			backtomenu.setVisible(true);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
 			backtomenu2.setVisible(false);
 			// Test:
 			System.out.println("You are in Main Menu");
@@ -58,7 +81,8 @@ public class mainMenu implements ActionListener{
 			panel.blnControls = false;
 			panel.blnPandT = true;
 			// Buttons:
-			play.setVisible(false);
+			server.setVisible(false);
+			client.setVisible(false);
 			help.setVisible(false);
 			backtocontrols.setVisible(true);
 			nexttopandt.setVisible(false);
@@ -73,7 +97,8 @@ public class mainMenu implements ActionListener{
 			panel.blnControls = true;
 			panel.blnPandT = false;
 			// Buttons:
-			play.setVisible(false);
+			server.setVisible(false);
+			client.setVisible(false);
 			help.setVisible(false);
 			backtocontrols.setVisible(false);
 			nexttopandt.setVisible(true);
@@ -87,7 +112,8 @@ public class mainMenu implements ActionListener{
 			panel.blnControls = false;
 			panel.blnPandT = false;
 			// Buttons:
-			play.setVisible(true);
+			server.setVisible(true);
+			client.setVisible(true);
 			help.setVisible(true);
 			backtocontrols.setVisible(false);
 			nexttopandt.setVisible(false);
@@ -99,68 +125,80 @@ public class mainMenu implements ActionListener{
 		if(e.getSource() == timer){
 			panel.repaint();
 		}		
-	}
-			
-	
+	}	
 	// Constructors
-	public mainMenu(){
+	public mainGameCode(){
 		frame = new JFrame("Tanks");
 		panel = new mainPanel();
 		panel.setPreferredSize(new Dimension(1280, 720));
 		panel.setLayout(null);
-		play = new JButton();
+		server = new JButton();
+		client = new JButton();
 		help = new JButton();
 		backtocontrols = new JButton();
 		nexttopandt = new JButton();
 		backtomenu = new JButton();
 		backtomenu2 = new JButton();
 		// Set Button Sizes:
-		play.setSize(300, 200);
-		help.setSize(120, 120);
+		server.setSize(300, 120);
+		client.setSize(300, 120);
+		help.setSize(100, 110);
 		backtomenu.setSize(120, 120);
 		backtocontrols.setSize(120, 120);
 		nexttopandt.setSize(120, 120);
 		backtomenu2.setSize(120, 120);
 		//Set Location (x, y):
-		play.setLocation(490, 260);
-		help.setLocation(95, 590);
+		server.setLocation(490, 382);
+		client.setLocation(490, 250);
+		help.setLocation(108, 590);
 		backtomenu.setLocation(12, 590);
 		backtocontrols.setLocation(12, 590);
 		nexttopandt.setLocation(1144, 590);
 		backtomenu2.setLocation(1144, 590);
 		// Button Feature Toggle:
-		// Play:
-		play.setBorder(null);
-		play.setContentAreaFilled(false);
-		play.setOpaque(false);
+		// Client:
+		//client.setBorder(null);
+		client.setContentAreaFilled(false);
+		client.setOpaque(false);
+		// Server:
+		//server.setBorder(null);
+		server.setContentAreaFilled(false);
+		server.setOpaque(false);
 		// Help:
 		help.setBorder(null);
 		help.setContentAreaFilled(false);
 		help.setOpaque(false);
 		// Back to Menu:
-		backtomenu.setBorder(null);
+		//backtomenu.setBorder(null);
 		backtomenu.setContentAreaFilled(false);
 		backtomenu.setOpaque(false);
-		backtomenu2.setBorder(null);
+		//backtomenu2.setBorder(null);
 		backtomenu2.setContentAreaFilled(false);
 		backtomenu2.setOpaque(false);
 		// Controls:
-		backtocontrols.setBorder(null);
+		//backtocontrols.setBorder(null);
 		backtocontrols.setContentAreaFilled(false);
 		backtocontrols.setOpaque(false);
 		// Powerup and Tiles:
-		nexttopandt.setBorder(null);
+		//nexttopandt.setBorder(null);
 		nexttopandt.setContentAreaFilled(false);
 		nexttopandt.setOpaque(false);
 		// Adding Buttons to Panel:
-		panel.add(play);
+		panel.add(server);
+		panel.add(client);
 		panel.add(help);
 		panel.add(backtomenu);
 		panel.add(backtocontrols);
 		panel.add(nexttopandt);
 		panel.add(backtomenu2);
+		// Preset these buttons to false so they dont show up
+		backtocontrols.setVisible(false);
+		nexttopandt.setVisible(false);
+		backtomenu.setVisible(false);
+		backtomenu2.setVisible(false);
 		// Adding ActionListener to Buttons:
-		play.addActionListener(this);
+		server.addActionListener(this);
+		client.addActionListener(this);
 		help.addActionListener(this);
 		backtomenu.addActionListener(this);
 		nexttopandt.addActionListener(this);
@@ -177,7 +215,6 @@ public class mainMenu implements ActionListener{
 	}
 	// Main Method
 	public static void main(String[] args){
-		new mainMenu();
+		mainGameCode mm = new mainGameCode();
 	}
 }
-
