237534face96370cecb2c7fc7565b0e6537f3e84
BLian <brianlian38@users.noreply.github.com>
2019-01-21 16:12:41 -0500

    Sorted Final Code
    Within the Final Folder are the final CPT components

---
+++ Finals/GameClient.java
@@ -0,0 +1,226 @@
+//imports
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import java.lang.Object.*;
+
+public class GameClient implements ActionListener, KeyListener, MouseListener, MouseMotionListener{
+	//---------------------------------------------------------------PROPERTIES----------------------------------------------------------------//
+	SuperSocketMaster ssm;
+	String IP; //set in constructor
+	JFrame frame;
+	GamePanel panel;
+	Timer timer;
+	String RecievedData = "127.0.0.1,forward,f";	//default values (just false, so nothing happens)
+	String[] split;
+	//Boolean clicked = false;	//for combining mouse/motion listeners, not used - bullets only fire on first click
+	String[] savedIPs = new String[4];
+	{Arrays.fill(savedIPs, "empty");}
+	Boolean isW = false;
+	Boolean isA = false;
+	Boolean isS = false;
+	Boolean isD = false;
+	
+	
+	//-----------------------------------------------------------------METHODS-----------------------------------------------------------------//
+	
+	//get, set variables with client ID number given (from split[0] in next)
+	public void ParseAndSet(String[] split){
+		int ClientNumber = Integer.parseInt(split[1]);	//split 0/1 are 'formatted'/the ID
+		for(int count = 2; count<split.length; count++){
+			//w
+			if(split[count].equals("forward")){
+				count++;
+				if(split[count].equals("t")){//forward true
+					panel.moveForwards[ClientNumber] = true;
+				} else {//forward false
+					panel.moveForwards[ClientNumber] = false;
+				}
+			//s
+			} else if(split[count].equals("back")){
+				count++;
+				if(split[count].equals("t")){
+					panel.moveBack[ClientNumber] = true;
+				} else {
+					panel.moveBack[ClientNumber] = false;
+				}
+			//a
+			} else if(split[count].equals("left")){
+				count++;
+				if(split[count].equals("t")){
+					panel.moveLeft[ClientNumber] = true;
+				} else {
+					panel.moveLeft[ClientNumber] = false;
+				}
+			//d
+			}  else if(split[count].equals("right")){
+				count++;
+				if(split[count].equals("t")){
+					panel.moveRight[ClientNumber] = true;
+				} else {
+					panel.moveRight[ClientNumber] = false;
+				}
+			//mouse clicked
+			} else if(split[count].equals("clicked")){		//format is IP,"clicked",mouseX,mouseY
+				panel.clicked[ClientNumber] = true;
+				count++;
+				panel.mouseX[ClientNumber] = Integer.parseInt(split[count]);
+				count++;
+				panel.mouseY[ClientNumber] = Integer.parseInt(split[count]);
+			} else if(split[count].equals("spacebar")){
+				panel.rightclicked[ClientNumber] = true;
+			}
+			//end of variables
+		}
+	}
+	
+
+	//ActionListener
+	public void actionPerformed(ActionEvent e){
+		//get Data from server - will be GUI info, use it to draw
+		if(e.getSource() == ssm){
+			RecievedData = ssm.readText();
+			split = RecievedData.split(",");
+			
+			//only parse if from server
+			if(split[0].equals("formatted")){
+				ParseAndSet(split);
+				System.out.println("Client received: "+RecievedData);
+				System.out.println("");
+			} else if (split[0].equals("playerID") && split[1].equals(IP)){
+				panel.ClientPlayer = Integer.parseInt(split[2]);
+			}
+			
+			
+		}
+		
+		if(e.getSource() == timer){
+			panel.repaint();
+		}
+	}
+	
+	//key
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyChar() == 'w'){
+			if(isW == false){
+				ssm.sendText(IP+",forward,t");
+				isW = true;
+			}
+		}
+		if(e.getKeyChar() == 's'){
+			if(isS == false){
+				ssm.sendText(IP+",back,t");
+				isS = true;
+			}
+		}
+		if(e.getKeyChar() == 'a'){
+			if(isA == false){
+				ssm.sendText(IP+",left,t");
+				isA = true;
+			}
+		}
+		if(e.getKeyChar() == 'd'){
+			if(isD == false){
+				ssm.sendText(IP+",right,t");
+				isD = true;
+			}
+		}
+	}
+	public void keyReleased(KeyEvent e){
+		//change to false
+		if(e.getKeyChar() == 'w'){
+			ssm.sendText(IP+",forward,f");
+			isW = false;
+		}
+		if(e.getKeyChar() == 's'){
+			ssm.sendText(IP+",back,f");
+			isS = false;
+		}
+		if(e.getKeyChar() == 'a'){
+			ssm.sendText(IP+",left,f");
+			isA = false;
+		}
+		if(e.getKeyChar() == 'd'){
+			ssm.sendText(IP+",right,f");
+			isD = false;
+		}
+		if(e.getKeyCode() == 32){
+			ssm.sendText(IP+",spacebar");
+		}
+	}
+	public void keyTyped(KeyEvent e){
+	}
+	
+	//mouse
+	//format is IP,"clicked",mouseX,mouseY	--> true or false not needed
+	public void mouseReleased(MouseEvent e){
+	}
+	public void mousePressed(MouseEvent e){
+		//right click
+		if(SwingUtilities.isRightMouseButton(e)){
+			//do anything here?
+		} else {
+		//sends location as well
+		ssm.sendText(IP+",clicked,"+(e.getX()-panel.xShift)+","+(e.getY()-panel.yShift));
+		}
+	}
+	public void mouseClicked(MouseEvent e){	//after release
+	}
+	public void mouseExited(MouseEvent e){	//leaves window
+	}
+	public void mouseEntered(MouseEvent e){	//enters window
+	}
+	public void mouseMoved(MouseEvent e){
+	}
+	public void mouseDragged(MouseEvent e){
+		//x, y
+		//clicked = true;
+	}
+
+	//---------------------------------------------------------------CONSTRUCTOR---------------------------------------------------------------//
+	public GameClient(){
+
+		frame = new JFrame("Client");
+			frame.addKeyListener(this);
+		panel = new GamePanel();
+			panel.setPreferredSize(new Dimension(1280,720));
+			panel.addMouseMotionListener(this);
+			panel.addMouseListener(this);
+		timer = new Timer(1000/60, this);
+			timer.start();
+
+		frame.setContentPane(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+		ssm = new SuperSocketMaster("127.0.0.1",1337, this);	//moved this to end - otherwise it begins before the server and doesn't connect
+		IP = ssm.getMyAddress();
+		ssm.connect();
+		ssm.sendText(IP+",up,f"); //send text to get server to assign this IP a player number
+		
+	}
+	
+	//debug (fake ip)
+	public GameClient(String fakeIP){
+		frame = new JFrame("Client");
+			frame.addKeyListener(this);
+		panel = new GamePanel();
+			panel.setPreferredSize(new Dimension(1280,720));
+			panel.addMouseMotionListener(this);
+			panel.addMouseListener(this);
+		timer = new Timer(1000/60, this);
+			timer.start();
+
+		frame.setContentPane(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+		ssm = new SuperSocketMaster("127.0.0.1",1337, this);	//moved this to end - otherwise it begins before the server and doesn't connect
+		IP = fakeIP;
+		ssm.connect();
+		ssm.sendText(IP+",up,f"); //send text to get server to assign this IP a player number
+	}
+}
---
+++ Finals/mainGameCode.java
@@ -0,0 +1,227 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainGameCode implements ActionListener{
+	// Properties
+	JFrame frame;
+	mainPanel panel;
+	JButton client; // Client Button in MM
+	JButton server; // Server Button in MM
+	JButton help; // ? in MM
+	JButton backtomenu; // Arrow Buttons that lead back to MM
+	JButton backtocontrols; // Arrow Button that lead to controls
+	JButton nexttopandt; // Arrow Button that leads to powerup and tiles
+	JButton backtomenu2; // Arrow Buttons that lead back to MM
+	Timer timer;
+		
+	// Methods
+	// Button Direct Interaction
+	public void actionPerformed(ActionEvent e){
+		// IN MAIN MENU:
+		if(e.getSource() == client){
+			// Insert code which leads to game client screen
+			System.out.println("Starting a client");
+			new GameClient();
+			// Disable Buttons
+			client.setVisible(false);
+			server.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
+			frame.setExtendedState(JFrame.ICONIFIED);
+		}else if(e.getSource() == server){
+			// Insert code which leads to game server screen
+			System.out.println("Starting a server");
+			ServerThread sT = new ServerThread();
+			Thread sThread = new Thread(sT);
+			sThread.start();
+			new GameClient();
+			// Disable Buttons
+			client.setVisible(false);
+			server.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
+			frame.setExtendedState(JFrame.ICONIFIED);
+		// If you press "?":	
+		}else if(e.getSource() == help){
+			// Screens:			
+			panel.blnMM = false;
+			panel.blnControls = true;
+			panel.blnPandT = false;
+			// Buttons:
+			server.setVisible(false);
+			client.setVisible(false);
+			server.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(true);
+			backtomenu.setVisible(true);
+			backtomenu2.setVisible(false);
+			// Test:
+			System.out.println("You are in Controls");
+			// If you press "<--" in controls:				
+		}else if(e.getSource() == backtomenu){
+			// Screens:			
+			panel.blnMM = true;
+			panel.blnControls = false;
+			panel.blnPandT = false;
+			// Buttons:
+			server.setVisible(true);
+			client.setVisible(true);
+			help.setVisible(true);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
+			// Test:
+			System.out.println("You are in Main Menu");
+			// If you press "-->" in controls:*/
+		}else if(e.getSource() == nexttopandt){
+			// Screens:			
+			panel.blnMM = false;
+			panel.blnControls = false;
+			panel.blnPandT = true;
+			// Buttons:
+			server.setVisible(false);
+			client.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(true);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(true);
+			// Test:
+			System.out.println("You are in Powerup and Tiles");
+			// If you press "<--" in powerup and tiles:
+		}else if(e.getSource() == backtocontrols){
+			// Screens:			
+			panel.blnMM = false;
+			panel.blnControls = true;
+			panel.blnPandT = false;
+			// Buttons:
+			server.setVisible(false);
+			client.setVisible(false);
+			help.setVisible(false);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(true);
+			backtomenu.setVisible(true);
+			backtomenu2.setVisible(false);
+			// Test:
+			System.out.println("You are in Powerup and Tiles");
+		}else if(e.getSource() == backtomenu2){
+			// Screens:			
+			panel.blnMM = true;
+			panel.blnControls = false;
+			panel.blnPandT = false;
+			// Buttons:
+			server.setVisible(true);
+			client.setVisible(true);
+			help.setVisible(true);
+			backtocontrols.setVisible(false);
+			nexttopandt.setVisible(false);
+			backtomenu.setVisible(false);
+			backtomenu2.setVisible(false);
+			// Test:
+			System.out.println("You are in Main Menu");
+		}
+		if(e.getSource() == timer){
+			panel.repaint();
+		}		
+	}	
+	// Constructors
+	public mainGameCode(){
+		frame = new JFrame("Tanks");
+		panel = new mainPanel();
+		panel.setPreferredSize(new Dimension(1280, 720));
+		panel.setLayout(null);
+		server = new JButton();
+		client = new JButton();
+		help = new JButton();
+		backtocontrols = new JButton();
+		nexttopandt = new JButton();
+		backtomenu = new JButton();
+		backtomenu2 = new JButton();
+		// Set Button Sizes:
+		server.setSize(300, 120);
+		client.setSize(300, 120);
+		help.setSize(100, 110);
+		backtomenu.setSize(120, 120);
+		backtocontrols.setSize(120, 120);
+		nexttopandt.setSize(120, 120);
+		backtomenu2.setSize(120, 120);
+		//Set Location (x, y):
+		server.setLocation(490, 382);
+		client.setLocation(490, 250);
+		help.setLocation(108, 590);
+		backtomenu.setLocation(12, 590);
+		backtocontrols.setLocation(12, 590);
+		nexttopandt.setLocation(1144, 590);
+		backtomenu2.setLocation(1144, 590);
+		// Button Feature Toggle:
+		// Client:
+		//client.setBorder(null);
+		client.setContentAreaFilled(false);
+		client.setOpaque(false);
+		// Server:
+		//server.setBorder(null);
+		server.setContentAreaFilled(false);
+		server.setOpaque(false);
+		// Help:
+		help.setBorder(null);
+		help.setContentAreaFilled(false);
+		help.setOpaque(false);
+		// Back to Menu:
+		//backtomenu.setBorder(null);
+		backtomenu.setContentAreaFilled(false);
+		backtomenu.setOpaque(false);
+		//backtomenu2.setBorder(null);
+		backtomenu2.setContentAreaFilled(false);
+		backtomenu2.setOpaque(false);
+		// Controls:
+		//backtocontrols.setBorder(null);
+		backtocontrols.setContentAreaFilled(false);
+		backtocontrols.setOpaque(false);
+		// Powerup and Tiles:
+		//nexttopandt.setBorder(null);
+		nexttopandt.setContentAreaFilled(false);
+		nexttopandt.setOpaque(false);
+		// Adding Buttons to Panel:
+		panel.add(server);
+		panel.add(client);
+		panel.add(help);
+		panel.add(backtomenu);
+		panel.add(backtocontrols);
+		panel.add(nexttopandt);
+		panel.add(backtomenu2);
+		// Preset these buttons to false so they dont show up
+		backtocontrols.setVisible(false);
+		nexttopandt.setVisible(false);
+		backtomenu.setVisible(false);
+		backtomenu2.setVisible(false);
+		// Adding ActionListener to Buttons:
+		server.addActionListener(this);
+		client.addActionListener(this);
+		help.addActionListener(this);
+		backtomenu.addActionListener(this);
+		nexttopandt.addActionListener(this);
+		backtocontrols.addActionListener(this);
+		backtomenu2.addActionListener(this);
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		timer = new Timer(1000/60, this);
+		timer.start();
+		
+	}
+	// Main Method
+	public static void main(String[] args){
+		mainGameCode mm = new mainGameCode();
+	}
+}
